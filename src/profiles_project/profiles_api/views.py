b0726e500ea3f18f58a2977870a7737984b01f6f
Samir <samnik_12345@hotmail.com>
2018-10-24 18:32:11 -0400

    Trying to launch on AWS with Victor

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from . import serializers
 
@@ -49,6 +50,26 @@ class HelloApiView(APIView):
         """Deletes an object"""
 
         return Response({"method": "delete"})
+
+
+class HelloViewSets(viewsets.ViewSet):
+    """Test API ViewSet."""
+
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewset = [
+            "Uses actions (list, create, retrieve, update, partial_update)",
+            "Automatically maps to URLs using Routers",
+            "Provides more functionality with less code.",
+
+        ]
+
+        return Response({"message": "Hello", "a_viewset": a_viewset})
+
+
+
+
 
 
 
